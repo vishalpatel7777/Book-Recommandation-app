d6da35420b440791ca3b26e1602072a648d08fe5
vishalpatel7777 <patelvishal14642@gmail.com>
2025-06-21 16:28:13 +0530

    axios.js fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-
-const  BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:1000/api/v1" : "https://bookmosaic.onrender.com/api/v1"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:1000/api/v1"
+    : "https://book-mosaic.onrender.com/api/v1";
 
 const api = axios.create({
-    baseURL: BASE_URL ,
-})
+  baseURL: BASE_URL,
+});
 
-
-export default api
+export default api;
